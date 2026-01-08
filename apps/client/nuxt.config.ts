a1c784897317fc3ddc3bd040204853ed2d774b28
beichenxx <beichenxx@126.com>
2026-01-08 10:45:06 +0800

    feat(client): 增加监听ip

--- apps/client/nuxt.config.ts
+++ apps/client/nuxt.config.ts
@@ -26,6 +26,10 @@ export default defineNuxtConfig({
   devtools: {
     enabled: true,
   },
+  devServer: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
   app: {
     head: {
       title: "Earthworm",
